b23f26e447635d9875383e4125abdd17f5c4579b
jk-5 <jeffreykog@me.com>
2013-08-24 13:59:05 +0200

    Maps will now automaticly be extracted from the mappack

--- src/jk_5/nailed/Nailed.java
+++ src/jk_5/nailed/Nailed.java
@@ -2,13 +2,20 @@ package jk_5.nailed;
 
 import com.google.common.eventbus.EventBus;
 import jk_5.nailed.command.CommandGroup;
+import jk_5.nailed.config.NailedConfig;
+import jk_5.nailed.config.helper.ConfigFile;
+import jk_5.nailed.config.helper.Configuration;
 import jk_5.nailed.groups.GroupAdmin;
 import jk_5.nailed.groups.GroupPlayer;
 import jk_5.nailed.groups.GroupRegistry;
+import jk_5.nailed.map.MapManager;
 import jk_5.nailed.multiworld.MultiworldManager;
 import jk_5.nailed.players.PlayerRegistry;
 import jk_5.nailed.teams.TeamRegistry;
 import net.minecraft.src.CommandHandler;
+import net.minecraft.src.DedicatedServer;
+
+import java.io.File;
 
 /**
  * TODO: Edit description
@@ -21,14 +28,23 @@ public class Nailed {
     public static final PlayerRegistry playerRegistry = new PlayerRegistry();
     public static final GroupRegistry groupRegistry = new GroupRegistry();
     public static final MultiworldManager multiworldManager = new MultiworldManager();
+    public static final ConfigFile config = new ConfigFile(new File("nailed.cfg")).setComment("Nailed main config file");
+    public static final MapManager mapManager = new MapManager();
+    public static DedicatedServer server;
 
-    public static void init(){
+    public static void init(DedicatedServer server){
+        Nailed.server = server;
+
+        server.setMOTD("Next game in 4 minutes");
+
         eventBus.register(new NailedEventListener());
         eventBus.register(playerRegistry);
 
         groupRegistry.registerGroup("player", new GroupPlayer());
         groupRegistry.registerGroup("admin", new GroupAdmin());
         groupRegistry.setDefaultGroup("player");
+
+        mapManager.setupSpawnMap();
     }
 
     public static void registerCommands(CommandHandler handler){
---
+++ src/jk_5/nailed/map/MapManager.java
@@ -0,0 +1,60 @@
+package jk_5.nailed.map;
+
+import jk_5.nailed.Nailed;
+import jk_5.nailed.util.FileUtils;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+/**
+ * TODO: Edit description
+ *
+ * @author jk-5
+ */
+public class MapManager {
+
+    private File mapPack = new File(Nailed.config.getTag("mappack").getTag("path").setComment("Path to the mappack file").getValue("mappack.zip"));
+    private File mapsFolder = new File("maps");
+    private Properties config;
+    private int mapId = 0;
+
+    public void readMapConfig(){
+        ZipInputStream stream = null;
+        boolean foundConfig = false;
+        try{
+            stream = new ZipInputStream(new FileInputStream(this.mapPack));
+            ZipEntry entry;
+            while((entry = stream.getNextEntry())!=null){
+                if(entry.getName().equals("mappack.cfg")){
+                    foundConfig = true;
+                    this.config = new Properties();
+                    this.config.load(stream);
+                }
+            }
+        }catch(IOException e){
+            e.printStackTrace();
+            System.exit(1);
+        }finally{
+            try{
+                if(stream != null) stream.close();
+            }catch(IOException e){}
+        }
+        if(!foundConfig){
+            System.err.println("Was not able to read the mappack.cfg file from the mappack file");
+            System.exit(1);
+        }
+    }
+
+    public void setupSpawnMap(){
+        File map = this.unpackMap();
+        Nailed.server.setFolderName("maps" + System.getProperty("file.seperator", "/") + map.getName());
+    }
+
+    public File unpackMap(){
+        return FileUtils.unzipMapFromMapPack(this.mapPack, this.mapsFolder, "map" + (this.mapId++));
+    }
+}
