426c287708f5bc973d4b35b1c342fe762eb8c1bb
jk-5 <jeffreykog@me.com>
2013-08-29 16:45:18 +0200

    Fixed setWinner instruction NPE

--- src/jk_5/nailed/map/gamestart/instructions/InstructionSetWinner.java
+++ src/jk_5/nailed/map/gamestart/instructions/InstructionSetWinner.java
@@ -3,7 +3,6 @@ package jk_5.nailed.map.gamestart.instructions;
 import jk_5.nailed.Nailed;
 import jk_5.nailed.map.gamestart.GameThread;
 import jk_5.nailed.map.gamestart.IInstruction;
-import jk_5.nailed.teams.Team;
 
 /**
  * TODO: Edit description
@@ -12,15 +11,15 @@ import jk_5.nailed.teams.Team;
  */
 public class InstructionSetWinner implements IInstruction {
 
-    private Team winner;
+    private String winner;
 
     @Override
     public void injectArguments(String arguments) {
-        this.winner = Nailed.teamRegistry.getTeam(arguments);
+        this.winner = arguments;
     }
 
     @Override
     public void execute(GameThread controller) throws InterruptedException {
-        controller.setWinner(this.winner);
+        controller.setWinner(Nailed.teamRegistry.getTeam(this.winner));
     }
 }
